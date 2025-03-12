9fb4d0dd7452518931c4a19ba92cadb272408466
Paul Andrew <spaulandrew7@gmail.com>
2025-03-12 23:51:06 +0530

    added model relations

--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,9 +1,22 @@
 import express from "express";
+import { createRoom, deleteRoom, getAllRooms, getRoom, updateRoom } from "../controllers/room.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.send("Hello this is rooms endpoint");
-})
+// CREATE
+router.post("/:hotelid", verifyAdmin, createRoom);
+
+// UPDATE
+router.put("/:id", verifyAdmin, updateRoom);
+
+// DELETE
+router.delete("/:id/:hotelid", verifyAdmin, deleteRoom);
+
+// GET
+router.get("/:id", verifyAdmin, getRoom);
+
+// GET ALL
+router.get("/", verifyAdmin, getAllRooms);
 
 export default router
